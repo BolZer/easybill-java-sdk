5eccd5d1948a426e0788b36d1934eb148f934f69
Jan Noehles <noehles@easybill.de>
2023-09-01 18:11:46 +0200

    Add new workflows (#1)
    * Setup CI Pipeline

--- lib/src/test/java/com/github/bolzer/easybill_java_sdk/LibraryTest.java
+++ lib/src/test/java/com/github/bolzer/easybill_java_sdk/LibraryTest.java
@@ -1,11 +1,17 @@
 package com.github.bolzer.easybill_java_sdk;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class LibraryTest {
-    @Test void someLibraryMethodReturnsTrue() {
+
+    @Test
+    void someLibraryMethodReturnsTrue() {
         Library classUnderTest = new Library();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+        assertTrue(
+            classUnderTest.someLibraryMethod(),
+            "someLibraryMethod should return 'true'"
+        );
     }
 }
