aaecf6ad4311b8598963c2b71de2f8affddb6486
Jan Nöhles <jan.noehles@easybill.de>
2023-09-02 08:04:45 +0200

    change group id

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.0"
+version = "0.1"
 
 plugins {
     id("java-library")
@@ -30,7 +30,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(20))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
