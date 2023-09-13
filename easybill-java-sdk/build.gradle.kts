b527c7389a7496053cbdc19ea2d2141c2610e199
Jan Nöhles <jan.noehles@easybill.de>
2023-09-13 05:42:01 +0200

    Update dependencies

--- easybill-java-sdk/build.gradle.kts
+++ easybill-java-sdk/build.gradle.kts
@@ -18,10 +18,10 @@ plugins {
     id("java-library")
     id("maven-publish")
     id("signing")
-    id("com.diffplug.spotless") version "6.20.0"
-    id("com.github.spotbugs") version "5.1.0"
-    id("org.checkerframework") version "0.6.30"
-    id("io.freefair.lombok") version "8.1.0"
+    id("com.diffplug.spotless") version "6.21.0"
+    id("com.github.spotbugs") version "5.1.3"
+    id("org.checkerframework") version "0.6.33"
+    id("io.freefair.lombok") version "8.3"
 }
 
 dependencies {
